eb9e2629e0688e5084a00ced2beb495db693d3af
Furkan Sezal <fsezal46@gmail.com>
2022-10-26 03:51:20 +0300

    uptaded erc20

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -4,13 +4,20 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
-    uint256 public s_maxSupply = 1000000000000000000000000;
+    uint256 public s_initialSupply = 1000000e18;
+    address private immutable i_owner;
 
     constructor()
         ERC20("GovernanceToken", "GT")
         ERC20Permit("GovernanceToken")
     {
-        _mint(msg.sender, s_maxSupply);
+        i_owner = msg.sender;
+        _mint(msg.sender, s_initialSupply);
+    }
+
+    modifier onlyOwner() {
+        require(i_owner == msg.sender, "Ownable: caller is not the owner");
+        _;
     }
 
     function _afterTokenTransfer(
@@ -21,7 +28,11 @@ contract GovernanceToken is ERC20Votes {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20Votes)
+        onlyOwner
+    {
         super._mint(to, amount);
     }
 
