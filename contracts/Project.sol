6e8d1091d7c091e9e678b388f6139c2ba7910404
AnandK-2024 <anandkumar@iitbhilai.ac.in>
2022-10-26 21:59:16 +0530

    ownerRegistration done

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -30,7 +30,7 @@ contract Project{
         ) public {
             require(!TrackStatus[msg.sender].OwnerRegistrationStatus,"Your registration has already done");
 
-            ProjectOwnerDetails[msg.sender]=Project_Owner_Details(Owner,Email,Country, true);
+            ProjectOwnerDetails[msg.sender]=Project_Owner_Details(Owner,Email,Country,);
             TrackStatus[msg.sender]=Status(true, false);
                 }
 
